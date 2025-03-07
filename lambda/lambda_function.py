b434576e2596fd157493c80ac41be0231fbcacfc
Rafaelb1986 <rafaelar@amazon.com>
2025-03-07 13:20:37 -0600

    Add files via upload

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -1,9 +1,4 @@
-# -*- coding: utf-8 -*-
 
-# This sample demonstrates handling intents from an Alexa skill using the Alexa Skills Kit SDK for Python.
-# Please visit https://alexa.design/cookbook for additional examples on implementing slots, dialog management,
-# session persistence, api calls, and more.
-# This sample is built using the handler classes approach in skill builder.
 import logging
 import ask_sdk_core.utils as ask_utils
 
@@ -23,6 +18,7 @@ from ask_sdk_model import Response
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
+# Loads the metadata for the APL document.
 APL_DOCUMENT_ID = "audioplayer"
 
 APL_DOCUMENT_TOKEN = "documentToken"
@@ -80,7 +76,6 @@ class LaunchRequestHandler(AbstractRequestHandler):
                     stream=Stream(
                         token="midnight_radio",
                         url=create_presigned_url("Media/Cinelax.mp3"),
-                        offset_in_milliseconds=0
                     ),
                     metadata=AudioItemMetadata(
                         title="Cinelax by Liborio Conti",
@@ -125,13 +120,6 @@ class PauseIntentHandler(AbstractRequestHandler):
     def handle(self, handler_input):
         speak_output = "Pausing Midnight Radio."
 
-        session_attr = handler_input.attributes_manager.session_attributes
-        audio_player_state = handler_input.request_envelope.context.audio_player
-        offset = audio_player_state.offset_in_milliseconds if audio_player_state else 0
-
-        # Store the offset in session attributes
-        session_attr["offset"] = offset
-
         response_builder = handler_input.response_builder
 
         # Check if APL is supported
@@ -163,12 +151,8 @@ class ResumeStopIntentHandler(AbstractRequestHandler):
         return is_intent_name("AMAZON.ResumeIntent")(handler_input)
 
     def handle(self, handler_input):
-        session_attr = handler_input.attributes_manager.session_attributes
-        offset = session_attr.get("offset", 0)  # Default to 0 if no offset is stored
 
         speak_output = "Resuming Midnight Radio."
-
-        logger.info(f"ResumeStopIntentHandler: Retrieved offset {offset} milliseconds")
 
         response_builder = handler_input.response_builder
 
@@ -195,7 +179,6 @@ class ResumeStopIntentHandler(AbstractRequestHandler):
                         stream=Stream(
                             token="midnight_radio",
                             url=create_presigned_url("Media/Cinelax.mp3"),
-                            offset_in_milliseconds=offset
                         ),
                         metadata=AudioItemMetadata(
                             title="Cinelax by Liborio Conti",
@@ -257,7 +240,6 @@ class StartOverIntentHandler(AbstractRequestHandler):
                     stream=Stream(
                         token="midnight_radio",
                         url=create_presigned_url("Media/Cinelax.mp3"),
-                        offset_in_milliseconds=0
                     ),
                     metadata=AudioItemMetadata(
                         title="Midnight Radio",
@@ -343,7 +325,6 @@ class UnhandledFeaturesIntentHandler(AbstractRequestHandler):
     """Handler for Unsupported Features."""
     def can_handle(self, handler_input):
         return (is_intent_name("AMAZON.LoopOnIntent")(handler_input)
-                or is_intent_name("AMAZON.NavigateHomeIntent")(handler_input)
                 or is_intent_name("AMAZON.RepeatIntent")(handler_input)
                 or is_intent_name("AMAZON.PreviousIntent")(handler_input)
                 or is_intent_name("AMAZON.NextIntent")(handler_input)
@@ -389,56 +370,6 @@ class SessionEndedRequestHandler(AbstractRequestHandler):
         return handler_input.response_builder.response
 
 
-class IntentReflectorHandler(AbstractRequestHandler):
-    """The intent reflector is used for interaction model testing and debugging.
-    It will simply repeat the intent the user said. You can create custom handlers
-    for your intents by defining them above, then also adding them to the request
-    handler chain below.
-    """
-    def can_handle(self, handler_input):
-        # type: (HandlerInput) -> bool
-        return ask_utils.is_request_type("IntentRequest")(handler_input)
-
-    def handle(self, handler_input):
-        # type: (HandlerInput) -> Response
-        intent_name = ask_utils.get_intent_name(handler_input)
-        speak_output = "You just triggered " + intent_name + "."
-
-        return (
-            handler_input.response_builder
-                .speak(speak_output)
-                # .ask("add a reprompt if you want to keep the session open for the user to respond")
-                .response
-        )
-
-
-class CatchAllExceptionHandler(AbstractExceptionHandler):
-    """Generic error handling to capture any syntax or routing errors. If you receive an error
-    stating the request handler chain is not found, you have not implemented a handler for
-    the intent being invoked or included it in the skill builder below.
-    """
-    def can_handle(self, handler_input, exception):
-        # type: (HandlerInput, Exception) -> bool
-        return True
-
-    def handle(self, handler_input, exception):
-        # type: (HandlerInput, Exception) -> Response
-        logger.error(exception, exc_info=True)
-
-        speak_output = "Sorry, I had trouble doing what you asked. Please try again."
-
-        return (
-            handler_input.response_builder
-                .speak(speak_output)
-                .ask(speak_output)
-                .response
-        )
-
-# The SkillBuilder object acts as the entry point for your skill, routing all request and response
-# payloads to the handlers above. Make sure any new handlers or interceptors you've
-# defined are included below. The order matters - they're processed top to bottom.
-
-
 sb = SkillBuilder()
 
 sb.add_request_handler(LaunchRequestHandler())
@@ -452,8 +383,5 @@ sb.add_request_handler(PlaybackFinishedHandler())
 sb.add_request_handler(UnhandledFeaturesIntentHandler())
 sb.add_request_handler(FallbackIntentHandler())
 sb.add_request_handler(SessionEndedRequestHandler())
-sb.add_request_handler(IntentReflectorHandler()) # make sure IntentReflectorHandler is last so it doesn't override your custom intent handlers
-
-sb.add_exception_handler(CatchAllExceptionHandler())
 
 lambda_handler = sb.lambda_handler()
